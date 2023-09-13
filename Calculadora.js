154a7c2adb520717f5d2dd4753dc690ac976bd6f
Carlos Jessé Coelho <tv.coelho01@gmail.com>
2023-09-13 17:11:28 -0300

    update versao-final da calculadora
    eu queria agradecer a todos que me apoiaram (brincadeira) mas foi muito legal

--- Calculadora.js
+++ Calculadora.js
@@ -7,3 +7,19 @@ function clean(){
     document.getElementById('resultado').innerHTML = "";
 }
 
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }else{
+        document.getElementById('resultado').innerHTML = "Insira sua conta"
+    }
+    if(resultado == '0/0'){
+        document.getElementById('resultado').innerHTML = '<img src="https://i.gifer.com/origin/00/00ae53a95ee7af0be395f5291d792c9a_w200.gif" alt="">'
+    }
+}
